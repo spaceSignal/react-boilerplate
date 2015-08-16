176a45d53e26f861e7aa0e068907b07c8f074959
Italo A. Casas <italoacasas@gmail.com>
2015-08-15 22:55:19 -0400

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+// Packages import
+import React from 'react';
+import Router, {Route} from 'react-router';
+// Components import
+import Dashboard from './components/DashboardComponents/Dashboard.jsx';
+
+const routes = (
+  <Route handler={Dashboard} >
+  </Route>
+)
