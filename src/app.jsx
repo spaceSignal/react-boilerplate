d281d448928fa7177bf3954e75ef608fc3804075
Italo A. Casas <italoacasas@gmail.com>
2015-08-16 05:29:47 -0400

    fixing old code...

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,3 @@
 // Packages import
 import React from 'react';
 import Router, {Route} from 'react-router';
-// Components import
-import Dashboard from './components/DashboardComponents/Dashboard.jsx';
-
-const routes = (
-  <Route handler={Dashboard} >
-  </Route>
-)
